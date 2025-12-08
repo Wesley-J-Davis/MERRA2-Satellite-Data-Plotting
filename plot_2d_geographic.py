0cad4e3d9e4d2ba08b7a554fab16c5c2df548977
Wesley Davis <60855573+Wesley-J-Davis@users.noreply.github.com>
2025-12-08 14:43:31 -0500

    Update plot_2d_geographic.py
    put file name in title for easy reference

--- plot_2d_geographic.py
+++ plot_2d_geographic.py
@@ -118,8 +118,8 @@ def create_overview_plot_geographic(ds, var_name, output_dir):
         plt.tight_layout()
         cbar = fig.colorbar(last_valid_im, ax=axes[:i+1], shrink=0.6, pad=0.02)
         cbar.set_label(f'{var_name}')
-    
-    plt.suptitle(f'{var_name} - All Channels (Geographic Projection)', y=0.98)
+    file_name = ds.GranuleID
+    plt.suptitle(f'{var_name} - All Channels (Geographic Projection)\n{file_name}', y=0.98)
     
     # Save
     filename = f'{var_name}_overview_geographic.png'
