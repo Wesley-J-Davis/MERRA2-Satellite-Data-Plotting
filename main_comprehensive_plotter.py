5c6159c43121fe617835faba6e5f71e2305b484a
Wesley Davis <60855573+Wesley-J-Davis@users.noreply.github.com>
2025-12-08 13:43:06 -0500

    Update main_comprehensive_plotter.py

--- main_comprehensive_plotter.py
+++ main_comprehensive_plotter.py
@@ -45,7 +45,7 @@ def main():
                         help='Tau value (00,06,12,18, returns _XX format)',
                         metavar='TAU',
                         default="")
-    parser.add_argument('-plots_3d', '--plots_3d', required=True, metavar='T/F',
+    parser.add_argument('-plots_3d', '--plots_3d', required=True, metavar='True/False',
                         default="False")
     
     args = parser.parse_args()
